33137895bdac86d8a4d24a577b5cb039c356dc22
DeNdyy1945 <88478851+Ddd1945@users.noreply.github.com>
2023-06-26 05:57:01 -0700

    Update MapScreen.tsx

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,3 +1,7 @@
+/**
+ * Screen that creates random location for drivers in Moscow vicinity
+ */
+
 import React, { useEffect } from 'react';
 import { View, Dimensions } from 'react-native';
 import { useLogging } from '../hooks/useLogging';
@@ -7,20 +11,26 @@ import i18n from '../config/localization';
 import styles from '../../styles/styles';
 
 const MapScreen: React.FunctionComponent<IStackScreenProps> = (props) => {
+    // logging for HomeScreen component
     const [logging] = useLogging('Map Screen');
     const { navigation, route } = props;
 
+    // Hook for logging
     useEffect(() => {
         logging.info({ navigation, route });
     }, [logging]);
 
+    // Set header consider to choosen language
     if (i18n.language === 'en') navigation.setOptions({ title: 'Map' })
     else navigation.setOptions({ title: 'Карта' })
 
+    // Data about choosen driver passed from HomeScreen' flatlist
     let data: string = route.params.data;
 
+    // Data used for map' aspect ratio
     const { width, height } = Dimensions.get('window');
 
+    // Setting up map's ratio and driver' position
     const ASPECT_RATIO = width / height;
     const LATITUDE_DELTA = 0.02;
     const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
@@ -31,12 +41,14 @@ const MapScreen: React.FunctionComponent<IStackScreenProps> = (props) => {
         longitudeDelta: LONGITUDE_DELTA,
     };
 
+    // Set an icon for vehicle's type
     if (data.includes('Cargo')) var url = require('../../assets/icons/cargo.png');
     else if (data.includes('Special Transport')) var url = require('../../assets/icons/special-transport.png');
     else var url = require('../../assets/icons/passenger-car.png');
 
+    // Return UI component with configured map
     return (
-        <View /*style={styles.container}*/>
+        <View>
             <MapView style={styles.map}
                 provider={PROVIDER_GOOGLE}
                 initialRegion={INITIAL_POSITION}>
@@ -51,4 +63,5 @@ const MapScreen: React.FunctionComponent<IStackScreenProps> = (props) => {
     );
 };
 
+// Export MapScreen
 export default MapScreen;
