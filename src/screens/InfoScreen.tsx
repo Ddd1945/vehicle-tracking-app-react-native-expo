c29732abc95b3d83ea87e7fb886927c0ed0af613
DeNdyy1945 <88478851+Ddd1945@users.noreply.github.com>
2023-06-26 05:35:14 -0700

    Update InfoScreen.tsx

--- src/screens/InfoScreen.tsx
+++ src/screens/InfoScreen.tsx
@@ -1,3 +1,7 @@
+/**
+ * Screen that contains information about specific driver with a given number and functionality to contact with him
+ */
+
 import React, { useEffect } from 'react';
 import { Text, View, Linking } from 'react-native';
 import { useLogging } from '../hooks/useLogging';
@@ -7,26 +11,36 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import styles from '../../styles/styles';
 
 const InfoScreen: React.FunctionComponent<IStackScreenProps> = (props) => {
+    // Logging to InfoScreen
     const [logging] = useLogging('Info Screen');
     const { navigation, route } = props;
 
+    // Hook for logging
     useEffect(() => {
         logging.info({ navigation, route });
     }, [logging]);
 
+    // Update language of the List Header consider to choosen language
     if (i18n.language === 'en') navigation.setOptions({ title: 'Information' })
     else navigation.setOptions({ title: 'Информация' })
 
-    let data: string;
+    // Info about choosen driver passed from HomeScreen' flatlist
+    let data: string = route.params.data;
 
-    data = route.params.data;
-
+    // For sake of simplicity every driver has the same phone number
+    // It's possible to assign different numbers for drivers in .json file and process it with other data.
     const phoneNumber: string = 'Enter driver number here';
+    
+    // Request that will be used to contact driver on whatsapp with a prepared text
     const request: string = 'http://api.whatsapp.com/send?text=' + i18n.t('request') + '&phone=' + phoneNumber;
 
+    // Enter given number in android keypad
     const makePhoneCall = () => Linking.openURL('tel: ' + phoneNumber);
+
+    // Contact driver with prepared text
     const sendWhatsappMessage = () => Linking.openURL(request);
 
+    // For buttons was used TouchableOpacity cause of easiness of styling it
     return (
         <View style={styles.container}>
             <View style={styles.item}>
@@ -50,4 +64,5 @@ const InfoScreen: React.FunctionComponent<IStackScreenProps> = (props) => {
     );
 };
 
+// Export InfoScreen to use it outside
 export default InfoScreen;
