1fa4aea512d5fba4bb39f434030415d34f57a132
DeNdyy1945 <88478851+Ddd1945@users.noreply.github.com>
2023-06-26 09:00:30 -0700

    Update Logging.ts

--- src/library/Logging.ts
+++ src/library/Logging.ts
@@ -1,3 +1,11 @@
+/**
+ * Logging class to provide feedback to developer
+ */
+
+/* User can pass message and namespace. Namespace allow to fugire out where messages are coming out from.
+ Also developer can pass in any object type for the message and the function will figure out how to log 
+ them properply based on whether or not it's a string.
+ */
 class Logging {
     private namespace = 'Client';
 
