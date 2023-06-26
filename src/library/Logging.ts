8887216863ab260eb6ab76336efd26664423df5e
DeNdyy1945 <88478851+Ddd1945@users.noreply.github.com>
2023-06-26 08:46:00 -0700

    Update Logging.ts

--- src/library/Logging.ts
+++ src/library/Logging.ts
@@ -1,27 +1,18 @@
-declare global { var isHomeJustLaunched: boolean }
-
 class Logging {
     private namespace = 'Client';
 
-    constructor(namespace?: string) {
-        if (namespace) this.namespace = namespace;
-    }
+    constructor(namespace?: string) { if (namespace) this.namespace = namespace; }
 
     public info = (message: any) => {
-        if (typeof message === 'string') {
+        if (typeof message === 'string') 
             console.log(`[${this.getDate()}] [${this.namespace}] [INFO] ${message}`);
-        } else {
+        else 
             console.log(`[${this.getDate()}] [${this.namespace}] [INFO]`, message);
-        }
     };
 
-    public getDate = () => {
-        return new Date().toISOString();
-    };
+    public getDate = () => { return new Date().toISOString(); };
 
-    public setNamespace = (namespace: string) => {
-        this.namespace = namespace;
-    };
+    public setNamespace = (namespace: string) => { this.namespace = namespace; };
 }
 
 export default Logging;
